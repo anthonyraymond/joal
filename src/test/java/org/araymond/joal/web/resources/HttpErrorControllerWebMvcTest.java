12aa47c56e2563e6fe81cca94d1e45a1eb7826b4
anthonyraymond <anthonyraymond@users.noreply.github.com>
2018-04-09 23:12:55 +0200

    Removed useless imports

--- src/test/java/org/araymond/joal/web/resources/HttpErrorControllerWebMvcTest.java
+++ src/test/java/org/araymond/joal/web/resources/HttpErrorControllerWebMvcTest.java
@@ -1,22 +1,10 @@
 package org.araymond.joal.web.resources;
 
-import org.araymond.joal.web.config.EndpointObfuscatorConfiguration;
-import org.araymond.joal.web.config.JacksonConfig;
-import org.araymond.joal.web.config.WebMvcConfiguration;
-import org.araymond.joal.web.config.WebSocketConfig;
-import org.araymond.joal.web.config.security.WebSecurityConfig;
-import org.araymond.joal.web.config.security.WebSocketAuthenticationSecurityConfig;
-import org.araymond.joal.web.config.security.WebSocketAuthorizationSecurityConfig;
-import org.araymond.joal.web.config.security.websocket.interceptor.AuthChannelInterceptorAdapter;
-import org.araymond.joal.web.config.security.websocket.services.WebSocketAuthenticatorService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.autoconfigure.web.BasicErrorController;
 import org.springframework.boot.autoconfigure.web.ErrorMvcAutoConfiguration;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.FilterType;
-import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
