8c35b60ebd533528d1c825b4c6324efb68380ef8
anthonyraymond <anthonyraymond@users.noreply.github.com>
2018-05-21 01:34:56 +0200

    Fix #29, client files are now sent ordered semantically

--- src/main/java/org/araymond/joal/core/client/emulated/BitTorrentClientProvider.java
+++ src/main/java/org/araymond/joal/core/client/emulated/BitTorrentClientProvider.java
@@ -1,6 +1,7 @@
 package org.araymond.joal.core.client.emulated;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.commons.io.FilenameUtils;
 import org.araymond.joal.core.SeedManager;
 import org.araymond.joal.core.config.JoalConfigProvider;
 import org.slf4j.Logger;
@@ -11,7 +12,9 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.Comparator;
 import java.util.List;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -33,9 +36,10 @@ public class BitTorrentClientProvider implements Provider<BitTorrentClient> {
     }
 
     public List<String> listClientFiles() {
-        try (Stream<Path> paths = Files.walk(this.clientsFolderPath)) {
+        try (final Stream<Path> paths = Files.walk(this.clientsFolderPath)) {
             return paths.filter(p -> p.toString().endsWith(".client"))
                     .map(p -> p.getFileName().toString())
+                    .sorted(new SemanticVersionFilenameComparator())
                     .collect(Collectors.toList());
         } catch (final IOException e) {
             throw new IllegalStateException("Failed to walk through .clients files", e);
@@ -69,5 +73,33 @@ public class BitTorrentClientProvider implements Provider<BitTorrentClient> {
         }
         this.bitTorrentClient = config.createClient();
         logger.debug("New client successfully generated.");
+    }
+
+    static final class SemanticVersionFilenameComparator implements Comparator<String> {
+
+        @Override
+        public final int compare(final String o1, final String o2) {
+            // remove file extension and replace '_' (which delimited build number with '.'
+            final String o1NameWithoutExtension = FilenameUtils.removeExtension(o1).replaceAll("_", ".");
+            final String o2NameWithoutExtension = FilenameUtils.removeExtension(o2).replaceAll("_", ".");
+
+            final String o1ClientName = o1NameWithoutExtension.substring(0, o1NameWithoutExtension.lastIndexOf('-'));
+            final String o2ClientName = o2NameWithoutExtension.substring(0, o2NameWithoutExtension.lastIndexOf('-'));
+
+            if (!o1ClientName.equalsIgnoreCase(o2ClientName)) {
+                return o1.compareTo(o2);
+            }
+
+            final String[] o1VersionSufix = o1NameWithoutExtension.substring(o1NameWithoutExtension.lastIndexOf('-') + 1, o1NameWithoutExtension.length()).split("\\.");
+            final String[] o2VersionSufix = o2NameWithoutExtension.substring(o2NameWithoutExtension.lastIndexOf('-') + 1, o2NameWithoutExtension.length()).split("\\.");
+            final int length = Math.max(o1VersionSufix.length, o2VersionSufix.length);
+            for(int i = 0; i < length; i++) {
+                final int thisPart = i < o1VersionSufix.length ? Integer.parseInt(o1VersionSufix[i]) : 0;
+                final int thatPart = i < o2VersionSufix.length ? Integer.parseInt(o2VersionSufix[i]) : 0;
+                if(thisPart < thatPart) return -1;
+                if(thisPart > thatPart) return 1;
+            }
+            return 0;
+        }
     }
 }
--- src/test/java/org/araymond/joal/core/client/emulated/BitTorrentClientProviderTest.java
+++ src/test/java/org/araymond/joal/core/client/emulated/BitTorrentClientProviderTest.java
@@ -1,6 +1,7 @@
 package org.araymond.joal.core.client.emulated;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.common.collect.Lists;
 import org.araymond.joal.core.SeedManager;
 import org.araymond.joal.core.config.JoalConfigProvider;
 import org.araymond.joal.core.config.JoalConfigProviderTest;
@@ -9,8 +10,11 @@ import org.mockito.Mockito;
 
 import java.io.FileNotFoundException;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
+import static org.assertj.core.api.Assertions.*;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
@@ -74,6 +78,49 @@ public class BitTorrentClientProviderTest {
         final List<String> clientFiles = provider.listClientFiles();
         assertThat(clientFiles).hasSize(1);
         assertThat(clientFiles.get(0)).isEqualTo("azureus-5.7.5.0.client");
+    }
+
+    @Test
+    public void comparatorShouldSortSemantically() {
+        final ArrayList<String> unordered = Lists.newArrayList(
+                "bittorrent-1.0.10.client",
+                "bittorrent-1.10.0.client",
+                "bittorrent-1.1.50.client",
+                "bittorrent-1.0.0.client",
+                "zz-atorrent-1.10.0.client",
+                "zz-atorrent-1.0.0.client",
+                "aa-ztorrent-1.10.0.client",
+                "aa-ztorrent-1.0.0.client",
+                "deluge-0.1.0.client",
+                "deluge-1.5.0.client",
+                "deluge-1.5.0.client",
+                "utorrent-3.5.0_44294.client",
+                "utorrent-3.5.0_43916.client",
+                "utorrent-3.5.0_44090.client"
+                );
+
+        final List<String> sorted = unordered.stream()
+                .sorted(new BitTorrentClientProvider.SemanticVersionFilenameComparator())
+                .collect(Collectors.toList());
+
+        assertThat(sorted).isEqualTo(
+                Lists.newArrayList(
+                        "aa-ztorrent-1.0.0.client",
+                        "aa-ztorrent-1.10.0.client",
+                        "bittorrent-1.0.0.client",
+                        "bittorrent-1.0.10.client",
+                        "bittorrent-1.1.50.client",
+                        "bittorrent-1.10.0.client",
+                        "deluge-0.1.0.client",
+                        "deluge-1.5.0.client",
+                        "deluge-1.5.0.client",
+                        "utorrent-3.5.0_43916.client",
+                        "utorrent-3.5.0_44090.client",
+                        "utorrent-3.5.0_44294.client",
+                        "zz-atorrent-1.0.0.client",
+                        "zz-atorrent-1.10.0.client"
+                        )
+        );
     }
 
 
