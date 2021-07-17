71409c235daec74bff67969c382a9c5f551ae02b
Anthony RAYMOND <anthonyraymond@users.noreply.github.com>
2021-07-17 09:54:45 +0200

    prepare release

--- publish.sh
+++ publish.sh
@@ -12,11 +12,6 @@ if ! grep -Fq "<version>$tagName</version>" ./pom.xml; then
   echo -e "$(tput setaf 1)WOW, wait! The provided tag does not match the pom.xml version$(tput sgr0)"
   exit 1
 fi
-if ! grep -Fq "JOAL_VERSION=\"$tagName\"" ./Dockerfile; then
-  echo -e "$(tput setaf 1)WOW, wait! The provided tag does not match the Dockerfile version$(tput sgr0)"
-  exit 1
-fi
-
 
 if GIT_DIR=./.git git rev-parse $tagName >/dev/null 2>&1
 then
